c94da8b533016b6efe6b362a27d1e1cbce51d59d
EstevanFelipe <estevanfelipesilva@gmail.com>
2021-12-31 05:30:46 -0300

    Tipos Primitivos
    Aprendizado sobre tipos primitivos e como eles apareceram impresso no console.

--- Tipos_Primitivos/main.js
+++ Tipos_Primitivos/main.js
@@ -3,22 +3,22 @@
 // Data deste conteúdo: 31/12/2021 - 5:20 AM
 
 // String
-console.log('String');
+console.log('String'); // String = impresso no console.
 // (String), variável que armazena textos e aFirmações.
 
 // Number
-console.log(12345);
+console.log(12345); // Number = impresso no console.
 // (Number), variável que armazena números.
 
 // Boolean
-console.log(true);
-console.log(false);
+console.log(true); // Boolean = impresso no console.
+console.log(false); // Boolean = impresso no console.
 // (Boolean), variável que armazena valores lógicos, true ou false.
 
 // Null
-console.log(null);
+console.log(null); // Null = impresso no console.
 // (Null), variável que armazena valores nulos.
 
 // Undefined
-console.log(undefined);
+console.log(undefined); // Undefined = impresso no console.
 // (Undefined), variável que armazena valores indefinidos.
