c382517e96007a33549a1fe10a8b2156eb09ab03
EstevanFelipe <estevanfelipesilva@gmail.com>
2021-12-31 05:28:27 -0300

    Tipos Primitivos
    aprendizado sobre tipos primitivos.

---
+++ Tipos_Primitivos/main.js
@@ -0,0 +1,24 @@
+// Tipos Primitivos em Javascript
+// Autor: Estevan Felipe
+// Data deste conteúdo: 31/12/2021 - 5:20 AM
+
+// String
+console.log('String');
+// (String), variável que armazena textos e aFirmações.
+
+// Number
+console.log(12345);
+// (Number), variável que armazena números.
+
+// Boolean
+console.log(true);
+console.log(false);
+// (Boolean), variável que armazena valores lógicos, true ou false.
+
+// Null
+console.log(null);
+// (Null), variável que armazena valores nulos.
+
+// Undefined
+console.log(undefined);
+// (Undefined), variável que armazena valores indefinidos.
