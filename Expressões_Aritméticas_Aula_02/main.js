139652b1ae21205196bbf64122ce21b92d50a64a
EstevanFelipe <estevanfelipesilva@gmail.com>
2021-12-31 21:17:27 -0300

    Expre Aritmética Aula 02
    operadores de expressões numéricas...

---
+++ Expressões_Aritméticas_Aula_02/main.js
@@ -0,0 +1,40 @@
+/*      -- Expressões Aritméticas --
+
+    -- Soma -- ( + ).
+    console.log(1 + 1); // 2 = impresso no console.
+
+    -- Subtração -- ( - ).
+    console.log(1 - 1); // 0 = impresso no console.
+
+    -- Multiplicação -- ( * ).
+    console.log(1 * 1); // 1 = impresso no console.
+
+    -- Divisão -- ( / ).
+    console.log(1 / 1); // 1 = impresso no console.
+
+    -- Resto -- ( % ).
+    console.log(1 % 1); // 0 = impresso no console.
+
+    -- Potência -- ( ** ).
+    console.log(1 ** 1); // 1 = impresso no console.
+
+*/
+
+// Exemplos de Expressões Aritméticas:
+const soma = 1 + 10;
+console.log(soma); // 2 = impresso no console.
+
+const subtracao = 10 - 1;
+console.log(subtracao); // 9 = impresso no console.
+
+const multiplicacao = 10 * 10;
+console.log(multiplicacao); // 100 = impresso no console.
+
+const divisao = 10 / 2;
+console.log(divisao); // 5 = impresso no console.
+
+const resto = 10 % 2;
+console.log(resto); // 0 = impresso no console.
+
+const potencia = 10 ** 2;
+console.log(potencia); // 100 = impresso no console.
