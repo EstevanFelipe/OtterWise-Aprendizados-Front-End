ecd2c4e7acbc3c9166fd9534da7a5693c1d6135c
EstevanFelipe <estevanfelipesilva@gmail.com>
2022-01-03 23:26:12 -0300

    Condicionais

---
+++ Aula_05/main.js
@@ -0,0 +1,15 @@
+/*
+
+            --- Condicionais ---
+
+            -- IF --
+            -- ELSE --
+            -- ELSE IF --
+            -- SWITCH CASE --
+
+    essas condicionais são usadas para fazer uma comparação entre dois valores,
+    se o valor for verdadeiro, ele executa um bloco de código, se não, ele ignora
+    o bloco de código, e segue a execução do código, basicamente é isso que as 
+    condicionais fazem.
+
+*/
