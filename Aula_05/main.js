64f9bbe84e58d840478bca7f723a5619a20387a9
EstevanFelipe <estevanfelipesilva@gmail.com>
2022-01-04 12:11:09 -0300

    Condicionais
    Conteúdo do main.js foi completado.

--- Aula_05/main.js
+++ Aula_05/main.js
@@ -13,3 +13,34 @@
     condicionais fazem.
 
 */
+
+a = "Estevan"; // Se a variável For Numerica, o console imprimira o valor Numerico,
+               // se não, ele imprimirá o valor inFormado no caso.
+
+if (a == 10) {
+    console.log('É valor númerico.');
+} else {
+    console.log('Não é um númerico.');
+}
+
+console.log(a)
+
+/* ------------------------------------------------------------------------------------------------ */
+
+value = "String";
+
+switch (value) {
+    case 1:
+        console.log(10);
+        break;
+    
+    case 2:
+        console.log('String');
+        break;
+    
+    default:
+        console.log('Não é nenhum dos valores acima.');
+        break;
+}
+
+console.log(value);
