1fa24a36afbd76d0a912c08500da87be855b7be1
EstevanFelipe <89105887+EstevanFelipe@users.noreply.github.com>
2022-01-06 11:59:50 -0300

    Exercitando um Pouco.
    Exercitando.

---
+++ Aula_09/main.js
@@ -0,0 +1,16 @@
+/* 
+
+            --- EXERCITANDO O CONTEÚDO APRENDIDO ATÉ AQUI!!! ---
+
+            // EXERCÍCIO 01 //
+
+            -- Crie uma Função chamada (checkSingal) que possui um parâmetro e imprima no console:
+
+    -- "X é positivo" se o número For positivo.
+
+    -- "X é negativo" se o número For negativo.
+
+            -- X deve ser substituido pelo número passado como argumento.
+               Realizar uma chamada da Função (chackSingal) passando o (Number -99) e outra com o (Number 42).
+
+*/
